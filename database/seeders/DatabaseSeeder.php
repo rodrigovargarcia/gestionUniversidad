8e17705a3d10cd8e464166712e1cc4e953d2434d
Rodrigo <rodrigovargarcia@gmail.com>
2023-12-01 09:43:53 -0300

    agregamos seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => '123rodrigo',
+        ]);
+
+        \App\Models\Carrera::create([
+            'nombre' => 'Ing Software',
+            'duracion_anios' => 5,
+        ]);
+
+        \App\Models\Alumno::create([
+            'nombre' => 'Fulano',
+            'apellido' => 'Perez',
+            'dni' => 1,
+            'telefono' => '+543855000000',
+            'legajo' => 1,
+            'estado' => 'activo',
+            'carrera_id' => 1,
+        ]);
+
+        \App\Models\Materia::create([
+            'nombre_materia' => 'Base de datos',
+            'horas_cursado' => 4,
+            'duracion' => 'cuatrimestral',
+            'carrera_id' => 1,
+        ]);
+
+        \App\Models\RegistroMateriaPorAlumno::create([
+            'materia_id' => 1,
+            'alumno_id' => 1,
+            'estado' => 'aprobado',
+            'fecha' => '2023-08-24',
+        ]);
     }
 }
